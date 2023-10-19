9e44e8749df18795117b9b872b2248a2c37922b9
headshigh <nischalgautam7200@gmail.com>
2023-10-19 20:25:01 +0545

    changed bundle.js

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve the 'bundle.js' file directly
 app.get("/bundle.js", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/bundle.js"));
 });
 
-// Serve your HTML file
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "dist/bundle.js"));
 });
--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import React from "react";
 import ChatBubble from "./components/Chatbubble";
 
 function App() {
   return (
     <div className="">
-      <div className="text-3xl">hello</div>
       <ChatBubble />
     </div>
   );
